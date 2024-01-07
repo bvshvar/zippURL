0115f394317dd2fa651c8a20b1b39ca7ee50687c
Bashar <basharf2014@gmai.com>
2024-01-07 17:37:18 -0500

    fixed main.py

--- zip-url-server/app/main.py
+++ zip-url-server/app/main.py
@@ -5,7 +5,7 @@ import os
 
 cred_obj = firebase_admin.credentials.Certificate('./ServiceAccountKey.json')
 default_app = firebase_admin.initialize_app(cred_obj,  {
-	'databaseURL': ''
+	'databaseURL': 'https://zipurl-3d497-default-rtdb.firebaseio.com/'
 	})
 
 app = Flask(__name__, static_folder='./build/static', template_folder="./build" )
